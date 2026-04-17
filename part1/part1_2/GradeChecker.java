b95eba65e87cf2b5c7019d11f208f3c787eee36d
Alyona-Soboleva <alena.soboleva.2006@mail.ru>
2026-04-17 19:22:56 +0300

    I complete task 1.2

--- part1/part1_2/GradeChecker.java
+++ part1/part1_2/GradeChecker.java
@@ -6,14 +6,34 @@ public class GradeChecker {
         // TODO: реализуйте вариант через классический switch.
         // Подсказка: удобно сначала вычислить "десяток" score / 10.
         // ▼ ВАШ КОД ЗДЕСЬ ▼
-        return "TODO";
+        int tens = score / 10;
+        switch (tens) {
+            case 10:
+            case 9:
+                return "Отлично (A)";
+            case 8:
+                return "Хорошо (B)";
+            case 7:
+                return "Удовлетворительно (C)";
+            case 6:
+                return "Слабо (D)";
+            default:
+                return "Неудовлетворительно (F)";
+        }
         // ▲ КОНЕЦ ВАШЕГО КОДА ▲
     }
 
     public static String getGradeArrow(int score) {
         // TODO: реализуйте вариант через switch -> (Java 14+).
         // ▼ ВАШ КОД ЗДЕСЬ ▼
-        return "TODO";
+        int tens = score / 10;
+        return switch (tens) {
+            case 10, 9 -> "Отлично (A)";
+            case 8 -> "Хорошо (B)";
+            case 7 -> "Удовлетворительно (C)";
+            case 6 -> "Слабо (D)";
+            default -> "Неудовлетворительно (F)";
+        };
         // ▲ КОНЕЦ ВАШЕГО КОДА ▲
     }
 
@@ -21,7 +41,7 @@ public class GradeChecker {
         int[] samples = {95, 85, 73, 62, 45, 100, 0};
         for (int score : samples) {
             System.out.printf("%d -> classic: %s | arrow: %s%n",
-                score, getGradeClassic(score), getGradeArrow(score));
+                    score, getGradeClassic(score), getGradeArrow(score));
         }
         // Проверь себя: для одного score оба метода дают одинаковую оценку.
     }
